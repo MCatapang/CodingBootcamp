ec070b87d26c9433b338089eb2e499be1dd9570c
MCatapang <maocatap@gmail.com>
2022-03-24 15:27:18 -0700

    Initial commit

---
+++ Week0/Notes_IntroToAlgoritms.js
@@ -0,0 +1,15 @@
+// Shorthand Notation
+    // Decrement
+        num = num -2
+        num -= 2
+    // Increment
+        num = num + 2
+        num += 2
+    // Decrement by 1
+        num = num - 1
+        num -= 1
+        num--
+    // Increment by 1
+        num = num + 1
+        num += 1
+        num++
