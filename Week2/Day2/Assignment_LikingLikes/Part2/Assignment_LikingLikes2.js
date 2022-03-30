a98d7efcd48763861de864ec4c8b44741fe650b9
MCatapang <maocatap@gmail.com>
2022-03-30 04:26:53 -0700

    Initial commit

---
+++ Week2/Day2/Assignment_LikingLikes/Part2/Assignment_LikingLikes2.js
@@ -0,0 +1,23 @@
+// LIKE BOX 1
+    var clicks1 = 9;
+
+    function onClick1() {
+        clicks1 += 1;
+        document.getElementById("clicks1").innerHTML = clicks1;
+    }
+
+// LIKE BOX 2
+    var clicks2 = 12;
+
+    function onClick2() {
+        clicks2 += 1;
+        document.getElementById("clicks2").innerHTML = clicks2;
+    }
+
+// LIKE BOX 3
+    var clicks3 = 9;
+
+    function onClick3() {
+        clicks3 += 1;
+        document.getElementById("clicks3").innerHTML = clicks3;
+    }
