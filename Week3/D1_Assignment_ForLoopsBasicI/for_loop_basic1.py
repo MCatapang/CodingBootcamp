87f118980316147271f16261be6cb7514e3c7a55
MCatapang <maocatap@gmail.com>
2022-04-04 14:15:17 -0700

    Initial commit

---
+++ Week3/D1_Assignment_ForLoopsBasicI/for_loop_basic1.py
@@ -0,0 +1,42 @@
+# 1. Integers from 0 to 150
+# count = 0
+# for count in range(0,151):
+#     print(count)
+
+# 2. Multiples of 5 from 5 to 100
+# count = 0
+# for count in range(5, 1001, 5):
+#     print(count)
+
+# # 3. Print Coding & Coding Dojo
+# count = 0
+# for count in range(1,101):
+#     if count%5 == 0:
+#         if count%10 ==0:
+#             print("Coding Dojo")
+#         else:
+#             print("Coding")
+#     else:
+#         print(count)
+
+# 4. Add Odd Integers
+# count = 0
+# odd_numbers = []
+# for count in range(0, 500001):
+#     if count%2 == 1:
+#         odd_numbers.append(count)
+# print(sum(odd_numbers))
+
+# # 5. Countdown by Fours
+# count = 2018
+# for count in range(2018, 0, -4):
+#     print(count)
+
+# 6. Flexible Counter
+# count = []
+# lowNum = 1
+# highNum = 100
+# mult = 6
+# for count in range(lowNum, highNum):
+#     if count%mult == 0:
+#         print(count)
