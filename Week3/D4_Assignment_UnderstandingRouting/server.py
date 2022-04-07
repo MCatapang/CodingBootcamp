9ce9b15e00f2684611149a33a65f8104e4ca21ec
MCatapang <maocatap@gmail.com>
2022-04-07 13:37:08 -0700

    Initial commit

---
+++ Week3/D4_Assignment_UnderstandingRouting/server.py
@@ -0,0 +1,39 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/dojo')
+def hello_dojo():
+    return 'Dojo!'
+
+@app.route("/say/<name>")
+def say_name(name):
+    return "Hello " + str(name).capitalize()
+
+
+@app.route("/repeat/<number>/<desired_word>")
+def repeat_string(number, desired_word):
+    return str(desired_word*int(number))
+
+@app.route("/<any>")
+def sorry_response(any):
+    if any:
+        return ("Sorry! No response. Try again.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
