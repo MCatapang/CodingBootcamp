9e50cad633910d2a318c1f4b59bb73055060aca2
MCatapang <maocatap@gmail.com>
2022-04-11 16:47:03 -0700

    Initial commit

---
+++ Week3/D5_Assignment_DojoSurvey/server.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, redirect, session
+app = Flask(__name__)  
+app.secret_key = 'and they were roommates'
+
+@app.route("/")
+def index():
+    session.clear()
+    return render_template("index.html")
+
+@app.route("/process", methods=['POST'])
+def process():
+    session["name"] = request.form["name"]
+    session["dojoLocation"] = request.form["dojoLocation"]
+    session["favoriteLanguage"] = request.form["favoriteLanguage"]
+    session["comments"] = request.form["comments"]
+    return redirect("/results")   
+
+@app.route("/results")
+def results():
+    return render_template("results.html")    
+
+if __name__=="__main__":   
+    app.run(debug=True)    
