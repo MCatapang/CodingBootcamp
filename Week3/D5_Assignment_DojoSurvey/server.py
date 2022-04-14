b289b74d042a0e68fb42225e86cb2b053469b6b1
MCatapang <maocatap@gmail.com>
2022-04-14 16:12:13 -0700

    Improved readability

--- Week3/D5_Assignment_DojoSurvey/server.py
+++ Week3/D5_Assignment_DojoSurvey/server.py
@@ -2,11 +2,16 @@ from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)  
 app.secret_key = 'and they were roommates'
 
+
+
+
+# DEFAULT ROUTE
 @app.route("/")
 def index():
     session.clear()
     return render_template("index.html")
 
+# SESSION ROUTE
 @app.route("/process", methods=['POST'])
 def process():
     session["name"] = request.form["name"]
@@ -15,9 +20,13 @@ def process():
     session["comments"] = request.form["comments"]
     return redirect("/results")   
 
+# RESULTS ROUTE
 @app.route("/results")
 def results():
     return render_template("results.html")    
+
+
+
 
 if __name__=="__main__":   
     app.run(debug=True)    
