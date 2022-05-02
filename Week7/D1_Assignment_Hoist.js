19426857d4b516aa258c29a09f9e785a810ec1d0
MCatapang <maocatap@gmail.com>
2022-05-02 16:09:48 -0700

    Testing some changes for git terminal practice

--- Week7/D1_Assignment_Hoist.js
+++ Week7/D1_Assignment_Hoist.js
@@ -250,5 +250,7 @@
         return dojo
     }
 
+    // Test change
+
     // Predicted Output
     // TypeError: Assignment to Constant Variable       // dojo is a constant; it's cant be redeclared; it's a dictionary, however, so its key-value pair can be modified.
