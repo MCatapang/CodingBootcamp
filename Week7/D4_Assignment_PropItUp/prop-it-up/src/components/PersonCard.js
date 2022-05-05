2ccebbaf5e17fa8f9f6038adacec45693a103efd
MCatapang <maocatap@gmail.com>
2022-05-05 10:37:27 -0700

    Renamed project folder

---
+++ Week7/D4_Assignment_PropItUp/prop-it-up/src/components/PersonCard.js
@@ -0,0 +1,20 @@
+// ------------------------IMPORTS------------------------
+import React, {Component} from 'react';
+
+
+// ------------------------COMPONENTS------------------------
+class PersonCard extends Component {
+    render() {
+        return (
+            <>
+                <h1 class="shadow">{this.props.lastName}, {this.props.firstName}</h1>
+                <p>Age: {this.props.age}</p>
+                <p>Hair Color: {this.props.hairColor}</p>
+            </>
+        )
+    }
+}
+
+
+// ------------------------EXPORT------------------------
+export default PersonCard;
