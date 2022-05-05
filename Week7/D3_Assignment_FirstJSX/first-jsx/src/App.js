23b49e3e3e625d06562b12a28065db4c3c4bc70e
MCatapang <maocatap@gmail.com>
2022-05-05 02:04:06 -0700

    Initial commit

---
+++ Week7/D3_Assignment_FirstJSX/first-jsx/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <list>
+          <li>Learmn React</li>
+          <li>Climb Mt. Everest</li>
+          <li>Run a marathon</li>
+          <li>Feed the dogs</li>
+      </list>
+    </div>
+  );
+}
+
+export default App;
