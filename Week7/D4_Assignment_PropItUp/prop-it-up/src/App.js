ce79f5e5cd3216f4410dc402fe1ef6e06938dcb8
MCatapang <maocatap@gmail.com>
2022-05-05 12:46:43 -0700

    Semicolon

--- Week7/D4_Assignment_PropItUp/prop-it-up/src/App.js
+++ Week7/D4_Assignment_PropItUp/prop-it-up/src/App.js
@@ -12,7 +12,7 @@ const PersonViewer = () => (
         < PersonCard lastName={"Fillmore"} firstName={"Millard"} age={50} hairColor={"Brown"} />
         < PersonCard lastName={"Smith"} firstName={"Maria"} age={62} hairColor={"Brown"} />
     </div>
-)
+);
 
 
 // ------------------------EXPORT------------------------
