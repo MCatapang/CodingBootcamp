6c4a55bcb897acf02b31c16252954e47e0d45fcd
MCatapang <maocatap@gmail.com>
2022-05-16 13:20:36 -0700

    Reorganized routes consisted with controller

--- Week8/D5_Assignment_JokesAPI/server/routes/jokes.routes.js
+++ Week8/D5_Assignment_JokesAPI/server/routes/jokes.routes.js
@@ -3,10 +3,10 @@ const JokesController = require('../controllers/jokes.controller');
 
 // ------------------------------------------------Export
 module.exports = app => {
-    app.get('/api/jokes', JokesController.findAllJokes);
-    app.get('api/jokes/:_id', JokesController.findOneJoke);
-    app.get('api/jokes/random', JokesController.findRandomJoke);
     app.post('/api/jokes/new', JokesController.createJoke);
-    app.put('/api/jokes/update/:_id', JokesController.updateJoke);
-    app.delete('/api/jokes/delete/:_id', JokesController.deleteJoke);
+    app.get('/api/jokes', JokesController.findAllJokes);
+    app.get('/api/jokes/:id', JokesController.findOneJoke);
+    app.get('/api/jokes/random', JokesController.findRandomJoke);
+    app.put('/api/jokes/update/:id', JokesController.updateJoke);
+    app.delete('/api/jokes/delete/:id', JokesController.deleteJoke);
 }
