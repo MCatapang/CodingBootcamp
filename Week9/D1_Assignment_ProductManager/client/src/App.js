3007aaa4342d8b909427dfa830e7c701ca1b10fd
MCatapang <maocatap@gmail.com>
2022-05-16 20:33:32 -0700

    Included Routes

--- Week9/D1_Assignment_ProductManager/client/src/App.js
+++ Week9/D1_Assignment_ProductManager/client/src/App.js
@@ -1,14 +1,22 @@
 // ------------------------------------------------Imports
 import Form from './components/Form';
-import {Route, Routes} from 'react-router-dom';
+import ProductList from './components/ProductList';
+import ProductDisplay from './components/ProductDisplay';
 import React from 'react';
+import {Routes, Route} from 'react-router-dom';
 import './App.css';
     
 
 // ------------------------------------------------App
 const App = () => {
     return (
-        <Form/>
+        <>
+            <Form/>
+            <Routes>
+                <Route path="/api/products/all" element={<ProductList/>} />
+                <Route path="/api/products/show/:id" element={<ProductDisplay/>} />
+            </Routes>
+        </>
     );
 }
 
