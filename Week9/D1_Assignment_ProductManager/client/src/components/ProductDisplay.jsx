1d5e0c4fa82973de151e0b4b2435557a1a11ed82
MCatapang <maocatap@gmail.com>
2022-05-16 20:33:56 -0700

    Initial commit

---
+++ Week9/D1_Assignment_ProductManager/client/src/components/ProductDisplay.jsx
@@ -0,0 +1,31 @@
+import axios from "axios";
+import React, {useState, useEffect} from 'react';
+import {useParams, Link} from "react-router-dom";
+import styles from './ProductDisplay.module.css';
+
+const ProductDisplay = (props) => {
+    const {id} = useParams();
+    const [product, setProduct] = useState(null);
+
+    useEffect( () => {
+        axios.get(`http://localhost:8000/api/products/${id}`)
+            .then( res => setProduct(res.data.Product[0]) )
+            .catch( err => console.log("You have an error: " + err) )
+    }, [id])
+
+    if(product !== null) {
+        return (
+            <>
+                <h1>{product.title}</h1>
+                <h3>Price: ${product.price}</h3>
+                <h3>{product.description}</h3>
+                <Link to='/api/products/all'>
+                    <button className={styles.btn}>Back to Product List</button>
+                </Link>
+            </>
+        )
+    }
+
+}
+
+export default ProductDisplay;
