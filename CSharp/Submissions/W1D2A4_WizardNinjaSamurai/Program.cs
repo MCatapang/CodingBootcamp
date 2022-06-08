1c762bca47d53517a32564c9c208c48543d9245c
MCatapang <maocatap@gmail.com>
2022-06-07 23:43:33 -0700

    Initial commit

--- CSharp/Submissions/W1D2A4_WizardNinjaSamurai/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿Human michaelangelo = new Human("Michael Angelo");
-Wizard raphael = new Wizard("Rafael");
-Ninja leonardo = new Ninja("Leonardo");
-Samurai donatello = new Samurai("Donatello");
-
-// raphael.Attack(michaelangelo).Attack(michaelangelo).Heal(raphael);
-// leonardo.Attack(michaelangelo).Attack(michaelangelo);
-donatello.FlowingBlade(michaelangelo).Attack(michaelangelo).Attack(raphael).Attack(leonardo).Meditate();
